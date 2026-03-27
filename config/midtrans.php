1a1b9fbe406ee2ce3aab56becf1126d4e764f99f
Mewyyii <ismimukaromah703.com>
2026-03-27 10:03:38 +0700

    4999

--- config/midtrans.php
+++ config/midtrans.php
@@ -6,5 +6,5 @@ return [
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
     'is_sanitized'  => env('MIDTRANS_IS_SANITIZED', true),
     'is_3ds'        => env('MIDTRANS_IS_3DS', true),
-    'pro_price'     => env('PRO_PRICE', 5000),
+    'pro_price'     => env('PRO_PRICE', 4999),
 ];
